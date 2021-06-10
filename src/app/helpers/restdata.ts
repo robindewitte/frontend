cb0554ef6f59a15f46b412ee277c838639dc4160
robindewitte <44872286+robindewitte@users.noreply.github.com>
2021-06-10 12:00:46 +0200

    Update restdata.ts

--- src/app/helpers/restdata.ts
+++ src/app/helpers/restdata.ts
@@ -3,7 +3,7 @@ import {HttpHeaders} from "@angular/common/http";
 export class Restdata {
 
   //local http://localhost:5001
-  private url = 'http://localhost:8123/';
+  private url = 'http://localhost:30001/';
 
   public httpOptionsToken = { 
         headers: new HttpHeaders().set('Authorization', 'Bearer ' +sessionStorage.getItem('token')).set('Content-Type', 'application/json') 
@@ -13,7 +13,7 @@ export class Restdata {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' +sessionStorage.getItem('token'),
-      'Access-Control-Allow-Origin': 'http://localhost:8123/',
+      'Access-Control-Allow-Origin': 'http://localhost:30001/',
       'Access-Control-Allow-Credentials': 'true'
     })
   };
